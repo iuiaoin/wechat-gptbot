8c8b86a69f70e7a62745f13ded48b61c39702b37
青麈 <zwluoqi@yeah.net>
2023-07-02 21:32:39 +0800

    stable画图服务

--- channel/wechat.py
+++ channel/wechat.py
@@ -13,6 +13,7 @@ from config import conf
 from utils.check import check_prefix, is_wx_account
 from common.reply import ReplyType
 import time
+import base64
 
 
 @singleton
@@ -29,6 +30,9 @@ class WeChatChannel:
             on_error=self.on_error,
             on_close=self.on_close,
         )
+        with open('aidraw.json', mode="r", encoding="utf-8") as f:
+            data = f.read()
+            self.aidraw = json.loads(data)
 
     def startup(self):
         logger.info("App startup successfully!")
@@ -125,28 +129,18 @@ class WeChatChannel:
         match_image_prefix = check_prefix(query, create_image_prefix)
         if match_image_prefix:
             context["type"] = const.CREATE_IMAGE
-        reply = ChatGPTBot().reply(query, context)
-        if reply.type == ReplyType.IMAGE:
-            self.send_img(reply.content, sender_id)
+        if match_image_prefix:
+            self.send_stable_img(query, sender_id)
         else:
-            reply_msg = self.build_msg(reply.content, wxid=sender_id)
-            self.ws.send(reply_msg)
+            reply = ChatGPTBot().reply(query, context)
+            if reply.type == ReplyType.IMAGE:
+                self.send_img(reply.content, sender_id)
+            else:
+                reply_msg = self.build_msg(reply.content, wxid=sender_id)
+                self.ws.send(reply_msg)
 
-    def send_img(self, content, wxid):
+    def send_image_path(self,img_path,wxid):
         try:
-            # download image
-            path = os.path.abspath("./assets")
-            img_name = int(time.time() * 1000)
-            response = requests.get(content, stream=True)
-            response.raise_for_status()  # Raise exception if invalid response
-
-            with open(f"{path}\\{img_name}.png", "wb+") as f:
-                for chunk in response.iter_content(chunk_size=8192):
-                    if chunk:  # filter out keep-alive new chunks
-                        f.write(chunk)
-                f.close()
-            img_path = os.path.abspath(f"{path}\\{img_name}.png").replace("\\", "\\\\")
-
             data = {
                 "id": gen_id(),
                 "type": const.PIC_MSG,
@@ -162,6 +156,76 @@ class WeChatChannel:
                 logger.info("image sent successfully")
             else:
                 logger.error(f"[Server Error]: {res.text}")
+        except Exception as e:
+            logger.error(f"[Download Image Error]: {e}")
+
+    def send_stable_img(self,tags,wxid):
+        try:
+            # download image
+            path = os.path.abspath("./assets")
+            img_name = int(time.time() * 1000)
+            # response = requests.get(content, stream=True)
+            # response.raise_for_status()  # Raise exception if invalid response
+
+            # print(self.aidraw)
+            # print(type(self.aidraw))
+            # print(type(self.aidraw['text2image']))
+
+            self.aidraw['text2image']['prompt'] = tags
+            self.aidraw['text2image']['seed'] = img_name
+            self.aidraw['text2image']['sampler_index'] = 'Euler a'
+            self.aidraw['text2image']['steps'] = 20
+            self.aidraw['text2image']['cfg_scale'] = 7
+
+
+            try:
+                headers = {'Content-Type': 'application/json', 'Authorization':''}
+                response = requests.post('http://127.0.0.1:7860/sdapi/v1/txt2img', data=json.dumps(self.aidraw['text2image']),headers=headers)
+                response.raise_for_status()  # 检查响应状态码
+
+                # 从响应中获取数据
+                json_data = response.json()
+                base64_image = json_data['images'][0].replace('data:image/png;', '').replace('base64,', '')
+                # 将base64编码的图像转换为字节数据
+                image_bytes = base64.b64decode(base64_image)
+    
+                with open(f"{path}\\{img_name}.png", "wb+") as f:
+                    f.write(image_bytes) # 返回请求到的JSON响应数据
+                    f.close()
+
+            except requests.exceptions.HTTPError as e:
+                print("HTTP请求错误:", e)
+                print("响应状态码:", response.status_code)
+                print("响应内容:", response.text)
+                return
+            except requests.exceptions.RequestException as e:
+                print("请求发生异常:", e)
+                return
+            except Exception as e:
+                print("发生错误:", e)
+                return
+
+            img_path = os.path.abspath(f"{path}\\{img_name}.png").replace("\\", "\\\\")
+            self.send_image_path(img_path,wxid)
+
+        except Exception as e:
+            logger.error(f"[Download Image Error]: {e}")
+
+    def send_img(self, content, wxid):
+        try:
+            # download image
+            path = os.path.abspath("./assets")
+            img_name = int(time.time() * 1000)
+            response = requests.get(content, stream=True)
+            response.raise_for_status()  # Raise exception if invalid response
+
+            with open(f"{path}\\{img_name}.png", "wb+") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+                f.close()
+            img_path = os.path.abspath(f"{path}\\{img_name}.png").replace("\\", "\\\\")
+            self.send_image_path(img_path,wxid)
         except Exception as e:
             logger.error(f"[Download Image Error]: {e}")
 
