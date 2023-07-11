831d3a6e37d51dfe4e249cd3e5db0d50d8296cfb
Xiaolin Liu <lixiaoli@microsoft.com>
2023-07-11 23:40:24 +0800

    misc

--- plugins/event.py
+++ plugins/event.py
@@ -33,7 +33,7 @@ class Event(BaseModel):
     action: EventAction = EventAction.PROCEED
 
     def __init__(self, type: EventType, data: dict):
-        super.__init__()
+        super().__init__()
         self.type = type
         self.channel = data.get("channel")
         self.message = data.get("message")
