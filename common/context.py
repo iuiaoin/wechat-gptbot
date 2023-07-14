6ed345036b789a4841b5f0ec714703e577803378
Xiaolin Liu <lixiaoli@microsoft.com>
2023-07-14 21:28:27 +0800

    misc

--- common/context.py
+++ common/context.py
@@ -15,4 +15,8 @@ class Context(BaseModel):
     session_id: str = None
     type: ContextType = ContextType.CREATE_TEXT
     query: str = None
-    system_prompt: str = conf().get("role_desc")
+    system_prompt: str = None
+
+    def __init__(self):
+        super().__init__()
+        self.system_prompt = conf().get("role_desc")
