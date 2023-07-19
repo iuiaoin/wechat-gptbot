d06d63e533094c04b0456dc426e2d2353c988dbe
Xiaolin Liu <lixiaoli@microsoft.com>
2023-07-19 19:22:49 +0800

    fix: update singleton

--- common/singleton.py
+++ common/singleton.py
@@ -2,9 +2,9 @@ def singleton(cls):
     instances = {}
 
     class Singleton(cls):
-        def __new__(cls, *args, **kwargs):
+        def __new__(cls):
             if cls not in instances:
-                instances[cls] = super(Singleton, cls).__new__(cls, *args, **kwargs)
+                instances[cls] = super(Singleton, cls).__new__(cls)
             return instances[cls]
 
     Singleton.__name__ = cls.__name__
