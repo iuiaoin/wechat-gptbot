b95fa3240540cecdcdc5ab57524126c7926a3994
Xiaolin Liu <lixiaoli@microsoft.com>
2023-07-19 19:36:24 +0800

    misc

--- bot/azure_chatgpt.py
+++ bot/azure_chatgpt.py
@@ -14,17 +14,6 @@ class AzureChatGPTBot(ChatGPTBot):
         self.args["deployment_id"] = conf().get("azure_deployment_id")
 
     def reply_img(self, query):
-        create_image_size = conf().get("create_image_size", "256x256")
-        try:
-            response = openai.Image.create(prompt=query, n=1, size=create_image_size)
-            image_url = response["data"][0]["url"]
-            logger.info(f"[ChatGPT] Image={image_url}")
-            return Reply(ReplyType.IMAGE, image_url)
-        except Exception as e:
-            logger.error(f"[ChatGPT] Create image failed: {e}")
-            return Reply(ReplyType.TEXT, "Image created failed")
-
-    def create_img(self, query):
         url = f"{openai.api_base}dalle/text-to-image?api-version=2022-08-03-preview"
         headers = {"api-key": openai.api_key, "Content-Type": "application/json"}
         create_image_size = conf().get("create_image_size", "256x256")
